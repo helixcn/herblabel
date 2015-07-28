d27d53264d0283cd165c3f22cf3a35ed381e1f08
Jin-Long Zhang <jinlongzhang01@gmail.com>
2015-07-28 14:13:42 +0800

    fixed date bug

--- R/compact_label_rtf.R
+++ R/compact_label_rtf.R
@@ -57,7 +57,10 @@ compact_label_rtf <- function(infile = NULL, outfile = "Wet_Labels.RTF",
         stop(paste("\"DATE_IDENTIFIED\" must be provided for row: ", 
              paste(which(is.na(herbdat000$DATE_IDENTIFIED)) + 1, collapse = ", ")))
         }
-    
+    formatdate <- function(x){
+        format(as.Date(herbdat$DATE_IDENTIFIED),"%d %B %Y")
+    }
+
     #################### 
     
     dirpgenus <- system.file("extdata", "APGIII_GENERA.csv", package = "herblabel")
@@ -170,8 +173,7 @@ compact_label_rtf <- function(infile = NULL, outfile = "Wet_Labels.RTF",
             paste("{\\pard\\keep\\keepn\\fi0\\li0\\sb80\\tqr\\tx5045\\qj ",
                    herbdat$COLLECTOR,", #" ,herbdat$COLLECTOR_NUMBER,
                    "                             ", 
-                   format(tryCatch(as.Date(herbdat$DATE_COLLECTED),
-                   error= function(e) {print("Date format incorrect, using original string");herbdat$DATE_COLLECTED}), "%d %B %Y"), 
+                   tryCatch(formatdate(herbdat$DATE_COLLECTED), error= function(e) {print("Warning: Date format incorrect, using original string"); herbdat$DATE_COLLECTED}), 
                    "\\qj0\\par}",sep = ""), 
             paste("{\\pard\\keep\\keepn\\fi0\\li0\\sb80\\tqr\\tx5045\\qj ",
                    herbdat$COLLECTOR,
@@ -180,8 +182,7 @@ compact_label_rtf <- function(infile = NULL, outfile = "Wet_Labels.RTF",
                    ", #",
                    herbdat$COLLECTOR_NUMBER, 
                    "                  ",
-                   format(tryCatch(as.Date(herbdat$DATE_COLLECTED),
-                   error= function(e) {print("Date format incorrect, using original string");herbdat$DATE_COLLECTED}), "%d %B %Y"),
+                   tryCatch(formatdate(herbdat$DATE_COLLECTED), error= function(e) {print("Warning: Date format incorrect, using original string"); herbdat$DATE_COLLECTED}),
                    "\\qj0\\par}",sep = "")
             ), 
         ##### COUNTY and LOCALITY
@@ -213,11 +214,15 @@ compact_label_rtf <- function(infile = NULL, outfile = "Wet_Labels.RTF",
         ##### IDENTIFICATION INFOMATION
         ifelse(!is.na(herbdat$TYPE_STATUS), 
             paste("{\\pard\\keep\\sa40\\keepn\\fi0\\li0\\tqr\\tx5045\\qr ", herbdat$TYPE_STATUS,
-                 "  Det.: ",herbdat$IDENTIFIED_BY,", ", format(tryCatch(as.Date(herbdat$DATE_IDENTIFIED), 
-                 error= function(e) {print("Date format incorrect, using original string");herbdat$DATE_IDENTIFIED}),"%d %B %Y"), " \\qr0\\par }",sep = ""),
+                 "  Det.: ",herbdat$IDENTIFIED_BY,", ", 
+                 tryCatch(formatdate(herbdat$DATE_IDENTIFIED), 
+                 error= function(e) {print("Warning: Date format incorrect, using original string"); herbdat$DATE_IDENTIFIED}), 
+                 " \\qr0\\par }",sep = ""),
             paste("{\\pard\\keep\\sa40\\keepn\\fi0\\li0\\tqr\\tx5045\\qr Det.: ",
-                 herbdat$IDENTIFIED_BY,", ", format(tryCatch(as.Date(herbdat$DATE_IDENTIFIED), 
-                 error= function(e) {print("Date format incorrect, using original string");herbdat$DATE_IDENTIFIED}),"%d %B %Y"), " \\qr0\\par }",sep = "")
+                 herbdat$IDENTIFIED_BY,", ",
+                 tryCatch(formatdate(herbdat$DATE_IDENTIFIED), 
+                 error= function(e) {print("Warning: Date format incorrect, using original string"); herbdat$DATE_IDENTIFIED}), 
+                 " \\qr0\\par }",sep = "")
             ),
         "{\\pard\\sa120 \\par }"
          )                            ### End of one label
