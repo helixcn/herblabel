eccf779baec5dd5e0ce1cce28e05c4d3417e0277
Jinlong Zhang <Jinlongzhang01@gmail.com>
2015-08-07 15:54:39 +0800

    updated the starting up message

--- R/OnAttach.R
+++ R/OnAttach.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname)  {
     Sys.setlocale("LC_TIME", "C")
-    packageStartupMessage(paste("This is herblabel ", packageVersion("herblabel"),"\n", sep = ""))
-    packageStartupMessage(paste("To input data, see Darwin Core Template: \n", system.file("extdata", "DARWIN_CORE_HERBARIUM_RECORDS_TEMPLATE.XLSX", package = "herblabel"),"\n", sep = ""))
-    packageStartupMessage(paste("To generate herbarium specimen labels, see the R script for a Working Example at: \n", system.file("extdata", "working_example_herblabel.R", package = "herblabel"),"\n", sep = ""))
+    packageStartupMessage(paste("This is R package herblabel ", packageVersion("herblabel"),"\n", sep = ""))
+    packageStartupMessage(paste("To input data, see Darwin Core Template at: \n", system.file("extdata", "DARWIN_CORE_HERBARIUM_RECORDS_TEMPLATE.XLSX", package = "herblabel"),"\n", sep = ""))
+    packageStartupMessage(paste("To generate herbarium specimen labels, see the R script for a Working Example at: \n", system.file("extdata", "working_example_herblabel.R", package = "herblabel"), sep = ""))
 }
