9228454f8fd119c5fe1f8405b96d1a199de5a853
Jinlong Zhang <jinlongzhang01@gmail.com>
2016-02-22 23:46:58 +0800

    on attatch

--- R/OnAttach.R
+++ R/OnAttach.R
@@ -1,5 +1,10 @@
 .onAttach <- function(libname, pkgname)  {
-    suppressMessages(Sys.setlocale("LC_TIME", "C"))
+
+    syst <- Sys.info()[['sysname']]
+    if(syst == "Windows"){
+        suppressMessages(Sys.setlocale("LC_TIME", "C"))
+    } 
+    
     options(stringsAsFactors = FALSE)
     options(scipen=999)
     packageStartupMessage(paste("This is herblabel ", packageVersion("herblabel"),".", sep = ""))
